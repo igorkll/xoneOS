f6325b0ebf101548ea1e94665303d71d7fe220cb
igorkll <frerther@gmail.com>
2024-02-02 22:06:45 +0300

    Update init.lua

--- files/init.lua
+++ files/init.lua
@@ -1,5 +1,7 @@
-local gpu = component.proxy(component.list()() or error("no GPU found", 2))
-local screen = component.list("screen")()
+computer.setArchitecture("Lua 5.3")
+
+local gpu = component.proxy(component.list("gpu")() or error("no GPU found", 2))
+local screen = component.list("screen")() or error("no screen found", 2)
 local rx, ry = gpu.getResolution()
 gpu.bind(screen)
 gpu.setBackground(0x000000)
@@ -7,11 +9,36 @@ gpu.setForeground(0xffffff)
 gpu.fill(1, 1, rx, ry, " ")
 component.invoke(screen, "turnOn")
 
+local component = component
+local computer = computer
+local unicode = unicode
+_G.component = nil
+_G.computer = nil
+_G.unicode = nil
+
 --------------------------------
 
-local xone = {}
-xone.version = "xoneOS"
+local xone = {
+    version = "xoneOS",
+    libs = {
+        component = component,
+        computer = computer,
+        unicode = unicode,
+        math = math,
+        table = table,
+        string = string
+    },
+    gpu = gpu,
+    screen = screen
+}
+xone.libs.xone = xone
 
 function xone.require(name)
-    
+    if xone.libs[name] then return xone.libs[name] end
+    error("library \"" .. name .. "\" not found", 2)
 end
+
+_OSVERSION = xone.version
+require = xone.require
+
+--------------------------------
